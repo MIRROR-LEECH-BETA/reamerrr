6208ebce66244f7f92bd2a2fa10897a6a5297ade
BETA official <109020708+MIRROR-LEECH-BETA@users.noreply.github.com>
2022-08-25 16:58:25 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -27,6 +27,29 @@ Send Me Any Files And Enjoyy""",
             )
         )
 
+@Client.on_message(filters.private & filters.command("start")) 
+async def start(bot, m: message):
+    mr = await bot.get_me()  
+    if not m.from_user:
+        return await m.reply_text("I don't know about you sar :(")
+    await add_user_to_database(c, m)
+    await c.send_flooded_message(
+        chat_id=m.chat.id,
+    await message.reply_photo(
+        photo=random.choice(PHOTO),
+        caption=f"""Hi {mr.mention}
+I Can Rename Files Without Downloading And Permanent Thumb Support.
+
+Send Me Any Files And Enjoyy""",
+        reply_markup=types.InlineKeyboardMarkup([[
+            InlineKeyboardButton("BOT OWNER", url="t.me/about_jeol"),
+            InlineKeyboardButton("UPDATES", url="https://t.me/Beta_BotZ")
+            ],[           
+            InlineKeyboardButton("SHOW SETTINGS", callback_data="showSettings"),
+            ]]
+            )
+        )
+
 @Client.on_message(filters.command("help") & filters.private & ~filters.edited)
 async def help_handler(c: Client, m: "types.Message"):
     if not m.from_user:
