06eb2432ad615009948e69da1320776ac1c5ab58
BETA official <109020708+MIRROR-LEECH-BETA@users.noreply.github.com>
2022-08-25 16:22:53 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -13,10 +13,10 @@ async def ping_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Hi, I am Rename Bot!\n\n"
+        text="Hi {message.from_user.mention}"
              "I can rename media without downloading it!\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.",
+             "With Custom Permanent Thumbnail Support\n\n"
+             "Just send me media and enjoy.",
         reply_markup=types.InlineKeyboardMarkup([[
            types.InlineKeyboardButton("Show Settings",
                                       callback_data="showSettings")
