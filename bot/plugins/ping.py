18bb8414c8d466b342bf4a002c173e38f607609a
BETA official <109020708+MIRROR-LEECH-BETA@users.noreply.github.com>
2022-08-25 17:03:04 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -6,35 +6,9 @@ from pyrogram.types import *
 from bot.core.db.add import add_user_to_database
 
 
-@Client.on_message(filters.command(["start", "ping"]) & filters.private & ~filters.edited)
-async def ping_handler(c: Client, m: "types.Message"):
-    mr = await m.get_me()
-    if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
-    await add_user_to_database(c, m)
-    await c.send_flooded_message(
-        chat_id=m.chat.id,
-        text=f"""Hi {mr.mention}
-I Can Rename Files Without Downloading And Permanent Thumb Support.
-
-Send Me Any Files And Enjoyy""",
-        reply_markup=types.InlineKeyboardMarkup([[
-            InlineKeyboardButton("BOT OWNER", ),
-            InlineKeyboardButton("UPDATES", url="https://t.me/Beta_BotZ")
-            ],[           
-            InlineKeyboardButton("SHOW SETTINGS", callback_data="showSettings"),
-            ]]
-            )
-        )
-
 @Client.on_message(filters.private & filters.command("start")) 
-async def start(bot, m: message):
-    mr = await bot.get_me()  
-    if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
-    await add_user_to_database(c, m)
-    await c.send_flooded_message(
-        chat_id=m.chat.id,
+async def start(bot, message):
+    mr = await bot.get_me() 
     await message.reply_photo(
         photo=random.choice(PHOTO),
         caption=f"""Hi {mr.mention}
