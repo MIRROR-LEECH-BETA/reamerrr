5fc790c82f3e55f5acd13bd54b69a84d75960138
Itz Me JEOL <102511853+Jeolpaul@users.noreply.github.com>
2022-12-04 21:20:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,14 +12,14 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "14417186"))
+    API_HASH = os.environ.get("API_HASH", "21731d919d79f78de24bdf1f6ccd6921")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5839828216:AAEUzWEa1U257TO7_AOJC_aFEHCMYz_UaX4")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 5558249587))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Giveaway:Giveaway@cluster0.0qa2d.mongodb.net/?retryWrites=true&w=majority")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001545756568"))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "True"))
