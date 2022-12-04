47ece85c5d295e92cc1de6f529de2a59360bf0c2
Itz Me JEOL <102511853+Jeolpaul@users.noreply.github.com>
2022-12-04 21:53:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -20,6 +20,6 @@ class Config(object):
     OWNER_ID = int(os.environ.get("OWNER_ID", "5558249587"))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Giveaway:Giveaway@cluster0.0qa2d.mongodb.net/?retryWrites=true&w=majority")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://LEECH:LEECH@cluster0.tfhutfu.mongodb.net/?retryWrites=true&w=majority")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001545756568"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "True"))
